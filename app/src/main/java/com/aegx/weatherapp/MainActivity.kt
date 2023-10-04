e1c87ba1ee433b94159b5f713ec349ffc785be73
maki <maknaitammar@gmail.com>
2023-10-04 10:21:17 +0200

    finish firste version

--- app/src/main/java/com/aegx/weatherapp/MainActivity.kt
+++ app/src/main/java/com/aegx/weatherapp/MainActivity.kt
@@ -11,7 +11,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
-    lateinit var tvReponse: TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,27 +21,6 @@ class MainActivity : AppCompatActivity() {
             .addToBackStack(null)
             .commit();
 
-//        tvReponse = findViewById(R.id.fragmentt_container)
-
-        //TODO: Call retrofit with API
-//        val retrofit: Retrofit = Retrofit.Builder()
-//            .baseUrl("https://api.openweathermap.org/data/2.5/weather/")
-//            .build();
-
-//        val weatherservice = retrofit.create(WeatherService::class.java);
-//        val res = weatherservice.getWeatherByCity();
-//
-//        res.enqueue(object: Callback<String> {
-//            override fun onResponse(call: Call<String>, response: Response<String>) {
-//                if (response.isSuccessful) {
-//                    tvReponse.text = response.body();
-//                }
-//            }
-//            override fun onFailure(call: Call<String>, t: Throwable) {
-//                Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show();
-//            }
-//
-//        })
 
     }
 }
